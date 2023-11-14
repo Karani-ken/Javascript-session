88db1e29bde3fff2f51788ea737530d8dd9aa953
Karani-Ken <academice79@gmail.com>
2023-11-14 11:56:36 +0300

    worked on arrays

---
+++ Advanced javascript/index.js
@@ -0,0 +1,102 @@
+const component = document.getElementById("app");
+
+component.innerHTML=`<h1>Hello Advanced Javascript</h1>`
+
+//Arrays
+//how to create an array
+const cars = ["volvo","subaru","Lexus"];
+const myCars = new Array("Volvo","subaru","Lexus");
+for(let i=0; i<cars.length;i++){
+    //accessing arrays
+    console.log(cars[i]);
+}
+//changing array elements
+cars[0] = "Ferrari";
+for(let i=0; i<cars.length;i++){
+    console.log(cars[i]);
+}
+var newParagraph = document.createElement('p');
+newParagraph.textContent = cars.toString();
+component.parentNode.insertBefore(newParagraph,component);
+
+//array properties and methods
+//array.length gives the length of an array
+console.log(cars.length); //3
+//looping through an array 
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+let element = document.createElement('ul');
+/*for(let i=0; i< fruits.length;i++){
+    let newLi = document.createElement('li');
+    newLi.textContent = fruits[0];
+    element.appendChild(newLi);
+}*/
+component.appendChild(element);
+
+//array foreach
+fruits.forEach(myFunction);
+function myFunction(value){
+    let newLi = document.createElement('li');
+    newLi.textContent = value;
+    element.appendChild(newLi);
+}
+component.appendChild(element);
+
+//adding elements into an array
+fruits.push("Pineapple");
+for(let i=0; i<fruits.length;i++){
+    console.log(fruits[i]);
+}
+//using the length property
+fruits[fruits.length] = "lemon";
+for(let i=0; i<fruits.length;i++){
+    console.log(fruits[i]);
+}
+
+//nb adding elements with high indexes creates undefined holes in the array
+/**
+ * fruits[9] = "passion fruit";
+ */
+
+//array methods
+//concantentaton
+const Girls = ["Njeri","Lucy","Breanna"];
+const Boys =["Kenny","Jimmy","Jamal"]
+
+const Friends = Girls.concat(Boys);
+Friends.forEach(myFunction);
+function myFunction(value){
+    let newLi = document.createElement('li');
+    newLi.textContent = value;
+    element.appendChild(newLi);
+}
+component.appendChild(element);
+/**
+ * const arr1 = ["Cecilie", "Lone"];
+const arr2 = ["Emil", "Tobias", "Linus"];
+const arr3 = ["Robin", "Morgan"];
+const myChildren = arr1.concat(arr2, arr3);
+ */
+
+//sorting arrays
+const numbers = [1,3,45,600,2.5,30,20,100,400,60,90,20,10];
+for(let i=0; i<numbers.length;i++){
+    console.log(numbers[i]);
+}
+numbers.sort(function(a,b) {return a-b});
+for(let i=0; i<numbers.length;i++){
+    console.log(numbers[i]);
+}
+var space = document.createElement('p');
+space.textContent = "Arrays";
+component.appendChild(space);
+numbers.sort(function(a,b) {return b - a});
+numbers.forEach(myFunction);
+function myFunction(value){
+    let newLi = document.createElement('li');
+    newLi.textContent = value;
+    element.appendChild(newLi);
+}
+component.appendChild(element);
+
+//random order sort
+//points.sort(function(){return 0.5 - Math.random()});
